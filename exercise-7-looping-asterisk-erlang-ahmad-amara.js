0245864dc346a14d13f8839e3c0d753f155fe1ed
erlangamara <erlang@Erlangs-MacBook-Pro.local>
2019-12-18 19:40:27 +0700

    upload tugas

---
+++ exercise-7-looping-asterisk-erlang-ahmad-amara.js
@@ -0,0 +1,30 @@
+// Nomor 1
+var rows1 = 5;
+do{
+    console.log('*');
+    rows1--;
+}
+while(rows1 > 0)
+
+// Nomor 2
+/*var result = '';
+for(rows2 = 5; rows2 > 0; rows2--){
+    
+    for(i = 0; i < 5; i++){
+        if (result.length === 5){
+            result = '';
+        }
+        result += '*';
+    }
+    console.log(result);
+}*/
+
+//Nomor 3
+/*var result = '';
+for(rows3 = 5; rows3 > 0; rows3--){
+    
+    for(i = 0; i < 1; i++){
+        result += '*';
+    }
+    console.log(result);
+}*/
